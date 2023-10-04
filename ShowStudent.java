7dc7d22f08bdbaf138d68ada0f094bcf920f9860
Jose Danielle Inocentes <113317506+offGr1d@users.noreply.github.com>
2023-10-04 11:32:32 +0800

    Update ShowStudent.java

--- ShowStudent.java
+++ ShowStudent.java
@@ -1,10 +1,17 @@
 public class ShowStudent{
    public static void main(String[] args){
       Student student1 = new Student(20152063, 3, 1.5);
+      Student student2 = new Student();
+
       
       student1.displayIdNumber();
       student1.displayCreditHoursEarned();
       student1.displayPointsEarned();
       student1.displayGradePointAverage();
+
+      student2.displayIdNumber();
+      student2.displayCreditHoursEarned();
+      student2.displayPointsEarned();
+      student2.displayGradePointAverage();
    }
 }
